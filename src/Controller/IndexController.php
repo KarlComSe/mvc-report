4651b0944d39ed5d681a9d91fce972465a2c1090
KarlComSe <karl@wackerberg.se>
2024-04-05 11:17:08 +0200

    Utilize quote service in index-page to present a quote

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,15 +2,40 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+# Generic use statements
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+
+# Specific use statements
+use App\Service\QuoteService;
+
 
 class IndexController extends AbstractController
 {
+
+    protected QuoteService $quoteService;
+
+    public function __construct(QuoteService $quoteService)
+    {
+        $this->quoteService = $quoteService;
+    }
+
     #[Route('/', name: 'app_home')]
     public function index()
     {
-        return $this->render('index.html.twig');
+        $quote = json_decode(
+            $this->quoteService->getAssadsQuote()->getContent(),
+            true
+        );
+        
+        $data = [
+            'quoteData' => [
+                'quote' => $quote['quote'],
+                'explanation' => $quote['explanation']
+            ]
+        ];
+
+        return $this->render('index.html.twig', $data);
     }
 }
