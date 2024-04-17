cec1acbfa798fe0a92914f48ee3d9e0591b9059e
KarlComSe <karl@wackerberg.se>
2024-04-17 11:32:34 +0200

    Add return statenent in getScore()

--- src/Model/CardHand.php
+++ src/Model/CardHand.php
@@ -53,7 +53,7 @@ class CardHand
                     $newScoreArray[] = $score + $value;
                 }
             }
-            $scoreArray = $newScoreArray;
+            return $newScoreArray;
         }
     }
 }
