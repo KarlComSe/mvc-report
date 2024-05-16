8062aa1e58baf511060418c67eee9d7fd8070197
KarlComSe <karl@wackerberg.se>
2024-05-16 15:04:51 +0200

    Add CRUD for currency entity

--- src/Controller/CurrencyController.php
+++ src/Controller/CurrencyController.php
@@ -2,17 +2,107 @@
 
 namespace App\Controller;
 
+use App\Entity\Currency;
+use App\Form\Type\CurrencyType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class CurrencyController extends AbstractController
 {
     #[Route('/currency', name: 'app_currency')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $currencies = $entityManager->getRepository(Currency::class)->findAll();
+
         return $this->render('currency/index.html.twig', [
             'controller_name' => 'CurrencyController',
+            'currencies' => $currencies,
+        ]);
+    }
+
+    #[Route('/currency/create', name: 'app_currency_create')]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $currency = new Currency();
+        $form = $this->createForm(CurrencyType::class, $currency);
+        $form->add('save', SubmitType::class, [
+            'label' => 'Create Currency',
+            'attr' => ['class' =>
+                'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-2 border border-blue-700 rounded']
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $currency = $form->getData();
+
+            $entityManager->persist($currency);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_currency');
+        }
+
+        return $this->render('currency/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+
+    #[Route('/currency/edit/{id}', name: 'app_currency_edit')]
+    public function edit(Request $request, EntityManagerInterface $entityManager, Currency $currency): Response
+    {
+        $form = $this->createForm(CurrencyType::class, $currency);
+        $form->add('save', SubmitType::class, [
+            'label' => 'Save edit',
+            'attr' => ['class' =>
+                'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-2 border border-blue-700 rounded']
+        ]);
+        $form->add('cancel', SubmitType::class, [
+            'label' => 'Cancel edit',
+            'attr' => ['class' =>
+                'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 my-2 border border-blue-700 rounded']
+        ]);
+
+
+        $form->handleRequest($request);
+
+        if ($form->get('cancel')->isClicked()) {
+            return $this->redirectToRoute('app_currency');
+        }
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $currency = $form->getData();
+
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_currency');
+        }
+
+        return $this->render('currency/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/currency/delete/{id}', name: 'app_currency_delete')]
+    public function delete(EntityManagerInterface $entityManager, Currency $currency): Response
+    {
+        $entityManager->remove($currency);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_currency');
+    }
+
+    #[Route('/currency/show/{id}', name: 'app_currency_show')]
+    public function show(Currency $currency): Response
+    {
+        return $this->render('currency/show.html.twig', [
+            'currency' => $currency,
         ]);
     }
 }
---
+++ src/Form/CurrencyType.php
@@ -0,0 +1,32 @@
+<?php
+
+// src/Form/Type/CurrencyType.php
+namespace App\Form\Type;
+
+use App\Entity\Currency;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CurrencyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('alpabethicCode', TextType::class)
+        ->add('decimalDigits', IntegerType::class)
+        ->add('entity', TextType::class)
+        ->add('numericCode', TextType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Currency::class,
+        ]);
+    }
+}
