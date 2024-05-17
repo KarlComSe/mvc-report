953dc2e80a32d4e06691d6691d92e7e4e8808876
KarlComSe <karl@wackerberg.se>
2024-05-17 17:04:31 +0200

    Remove unused import

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -8,7 +8,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Attribute\AsController;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Model\Game;
 
