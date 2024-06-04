34a92f1e2403dac4b5fbc521810ab0b48cb43e36
KarlComSe <karl@wackerberg.se>
2024-06-04 19:42:04 +0200

    add feature to view basic organization details for a logged in user

--- src/Controller/Kassabok/OrganizationController.php
+++ src/Controller/Kassabok/OrganizationController.php
@@ -2,18 +2,39 @@
 
 namespace App\Controller\Kassabok;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Organization;
+use Symfony\Bundle\SecurityBundle\Security;
 
 class OrganizationController extends AbstractController
 {
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     #[Route('/proj/organization', name: 'kassabok_organization')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
         $this->denyAccessUnlessGranted('ROLE_USER');
+
+        // $organizations = $entityManager->getRepository(Organization::class)->findAll();
+
+        // // Filter organizations based on user membership (if not admin)
+        // if (!$this->security->isGranted('ROLE_ADMIN')) {
+        //     $currentUser = $this->security->getUser();
+        //     $organizations = array_filter($organizations, function (Organization $organization) use ($currentUser) {
+        //         return $organization->getUsers()->contains($currentUser);
+        //     });
+        // }
         return $this->render('kassabok/organization/index.html.twig', [
             'controller_name' => 'OrganizationController',
+            'organizations' => $entityManager->getRepository(Organization::class)->findByUser($this->getUser()),
         ]);
     }
 }
--- src/Repository/OrganizationRepository.php
+++ src/Repository/OrganizationRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Organization;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\User;
 
 /**
  * @extends ServiceEntityRepository<Organization>
@@ -14,6 +15,25 @@ class OrganizationRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Organization::class);
+    }
+
+    public function findByUser(User $user): array
+    {
+        return $this->createQueryBuilder('o')
+            ->join('o.users', 'u')
+            ->where('u = :user')
+            ->setParameter('user', $user)
+            ->getQuery()
+            ->getResult();
+
+
+        // // Filter organizations based on user membership (if not admin)
+        // if (!$this->security->isGranted('ROLE_ADMIN')) {
+        //     $currentUser = $this->security->getUser();
+        //     $organizations = array_filter($organizations, function (Organization $organization) use ($currentUser) {
+        //         return $organization->getUsers()->contains($currentUser);
+        //     });
+        // }
     }
 
     //    /**
