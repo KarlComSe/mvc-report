0bf5cc50fa609e47435f2523248c76d8019b3204
KarlComSe <karl@wackerberg.se>
2024-06-07 11:08:06 +0200

    add form for organization incl. handling the model

--- src/Controller/Kassabok/OrganizationController.php
+++ src/Controller/Kassabok/OrganizationController.php
@@ -4,9 +4,12 @@ namespace App\Controller\Kassabok;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Organization;
+use App\Form\OrganizationFormType;
 use Symfony\Bundle\SecurityBundle\Security;
 
 class OrganizationController extends AbstractController
@@ -19,22 +22,45 @@ class OrganizationController extends AbstractController
     }
 
     #[Route('/proj/organization', name: 'kassabok_organization')]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         $this->denyAccessUnlessGranted('ROLE_USER');
 
-        // $organizations = $entityManager->getRepository(Organization::class)->findAll();
+        $organization = new Organization();
+        $form = $this->createForm(OrganizationFormType::class);
+        $form->add('save', SubmitType::class, [
+            'label' => 'Create organization']);
+        $form->handleRequest($request);
 
-        // // Filter organizations based on user membership (if not admin)
-        // if (!$this->security->isGranted('ROLE_ADMIN')) {
-        //     $currentUser = $this->security->getUser();
-        //     $organizations = array_filter($organizations, function (Organization $organization) use ($currentUser) {
-        //         return $organization->getUsers()->contains($currentUser);
-        //     });
-        // }
+        if ($form->isSubmitted() && $form->isValid()) {
+            $organization = $form->getData();
+            $organization->addUser($this->getUser());
+
+            $entityManager->persist($organization);
+            $entityManager->flush();
+            return $this->redirectToRoute('kassabok_organization');
+        }
+
         return $this->render('kassabok/organization/index.html.twig', [
             'controller_name' => 'OrganizationController',
             'organizations' => $entityManager->getRepository(Organization::class)->findByUser($this->getUser()),
+            'form' => $form,
         ]);
+    }
+
+    public function new(Request $request): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        $organization = new Organization();
+
+        $form = $this->createForm(OrganizationFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $organization = $form->getData();
+
+            return $this->redirectToRoute('kassabok_organization');
+        }
     }
 }
---
+++ src/Form/OrganizationFormType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Organization;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OrganizationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Organization Name',
+                'attr' => ['placeholder' => 'Enter the name of the organization'],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Organization::class,
+        ]);
+    }
+}
